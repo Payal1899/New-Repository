bc50bdf882b43f61f3f59abab71549581cd7b1ef
Payal1899 <payallute9@gmail.com>
2026-04-12 18:35:08 +0000

    New files

---
+++ Flasks/Backend/Api4.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+from dotenv import load_dotenv
+import os
+import pymongo
+
+load_dotenv() #calling function to load uri stored in .env file
+MONGO_URI = os.getenv('uri') # same variable which is passed in .env file
+client = pymongo.MongoClient(MONGO_URI)#created a client by providing the URI
+
+db = client.Assignment4 #created DB named 'Assignment2' in cluster name mentioned in URI
+
+collection = db['Assignment']#created collection named 'Assignment'
+
+app= Flask(__name__) #creating flask api
+
+@app.route('/submittodoitem', methods=['POST'])
+def submittodoitem():
+    #inserting into DB
+    dict1=dict(request.json)
+    collection.insert_one(dict1)
+        
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8000, debug=True) #running frontend and backend on different ports
